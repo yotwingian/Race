f072f9769bd17e32c92c675fabca689c77f1e5af
yotwingian <pawel.bielawski@hotmail.com>
2024-02-04 12:24:48 +0100

    add players now also add personal names

--- Play_Board.py
+++ Play_Board.py
@@ -5,21 +5,20 @@ def add_players():
     while True:
         answer = input("How many players would like to play? (max 5pl)")
         try:
-            answer = int(answer)
-            if 1 <= answer <= 5:
-                for player in range(answer):
-                    players.append({"name": f"Player {player + 1}"})
-
-                return players, print(players)
+            nr_players = int(answer)
+            if 1 <= nr_players <= 5:
+                for player in range(nr_players):
+                    name = input(f"Enter name for Player {player + 1}: ")
+                    players.append({"name": name})
+                return players, nr_players, print(players)
             else:
-                print(f"{answer}is invalid number. Please enter a number between 1 and 5")
+                print(f"{nr_players}is invalid number. Please enter a number between 1 and 5")
         except ValueError:
             print(f"{answer} is not a number. Please enter a number between 1 and 5")
 
 
 add_players()
-def draw_track():
-    pass
+
 def start_game():
     pass
 
