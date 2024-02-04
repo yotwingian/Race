11c09cad9c284193e8d787616068250b8648a664
yotwingian <pawel.bielawski@hotmail.com>
2024-02-04 18:11:09 +0100

    added draw_track and print track

--- Play_Board.py
+++ Play_Board.py
@@ -1,5 +1,8 @@
+import numpy as np
 def play_game():
     pass
+
+
 def add_players():
     players = []
     while True:
@@ -10,19 +13,38 @@ def add_players():
                 for player in range(nr_players):
                     name = input(f"Enter name for Player {player + 1}: ")
                     players.append({"name": name})
-                return players, nr_players, print(players)
+                return players, nr_players,
             else:
                 print(f"{nr_players}is invalid number. Please enter a number between 1 and 5")
         except ValueError:
             print(f"{answer} is not a number. Please enter a number between 1 and 5")
 
 
-add_players()
+players, nr_players = add_players() # för att få ut dessa variablar ur funktionen
+
+
+def draw_track(players):
+    track_length = 30
+    player_tracks = {}
+    for player in players:
+        track = np.full(track_length, '-')
+        track[3] = 'X'
+        player_tracks[player["name"]] = track
+    return player_tracks
+
+
+def print_tracks(player_tracks):
+    for player, track in player_tracks.items():
+        # Replace the player's current position with 'X'
+        print(f"{player}: {''.join(track)}")
+
+
+player_tracks = draw_track(players)
+print_tracks(player_tracks)
+
 
 def start_game():
     pass
-
-
 
 
 
